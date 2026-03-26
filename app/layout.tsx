2c8f6256758151ef39c148828b61620d0201d0ff
Suyash Tiwari <142603779+suyashrcb15@users.noreply.github.com>
2026-03-26 18:04:53 +0530

    Update app/layout.tsx to add space before equals sign

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,8 @@
-import type { Metadata } from "next";
-import {Geist, Geist_Mono, IBM_Plex_Serif, Mona_Sans} from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import {ClerkProvider} from "@clerk/nextjs";
-import {Toaster} from "sonner";
+import { IBM_Plex_Serif } from 'next/font/google';
 
-const ibmPlexSerif= IBM_Plex_Serif({
-    variable: "--font-ibm-plex-serif",
-    subsets: ['latin'],
-    weight:['400', '500', '600', '700'],
-    display: 'swap'
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
 });
 
-
-const monaSans= Mona_Sans({
-    variable: "--font-mona-sans",
-    subsets: ['latin'],
-    display: 'swap'
-})
-
-export const metadata: Metadata = {
-  title: "EchoBooks",
-  description: "EchoBooks is an audiobook app that lets users listen to books anytime, anywhere with immersive audio narration.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-      <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${ibmPlexSerif.variable} ${monaSans.variable} relative font-sans antialiased`}
-      >
-      <Navbar />
-        {children}
-      <Toaster richColors toastOptions={{}} />
-      </body>
-    </html>
-      </ClerkProvider>
-  );
-}
+export default ibmPlexSerif;
